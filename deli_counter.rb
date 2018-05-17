81455dfd2c262911e2056b13d5e71b073c8e268e
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 21:42:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,8 @@ def line(current_line)
   position_in_line = "The line is currently: "
   if current_line.length > 0 
     current_line.each_with_index do |name, position|
-    position_in_line += "#{position+1}. #{name} "
+    position_in_line += "#{position+1}. #{name}"
+    position != current_line.length-1 ? position_in_line += " " : 
   end
   puts position_in_line
   else
