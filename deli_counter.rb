8b4e756dce7b2fdf60745a377d37ec10820888bd
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 21:38:13 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,11 +1,12 @@
 # Write your code here.
 def line(current_line)
+  position_in_line = "The line is currently: "
   if current_line.length > 0 
     current_line.each_with_index do |name, position|
-    puts name
-    puts position+1
+    position_in_line += "#{position+1}. #{name} "
   end
   else
     puts "The line is currently empty."
-  end
+end
+puts position_in_line
 end
