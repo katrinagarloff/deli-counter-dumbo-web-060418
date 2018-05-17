567c9b9fb45611f1cf0583768c7b45e9d551c351
Katrina Garloff <katrinagarloff@gmail.com>
2018-05-17 21:32:34 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,5 +1,10 @@
 # Write your code here.
 def line(current_line)
   if current_line.length > 0 
-    current_line.each_with_index
+    current_line.each_with_index |name, position|
+    puts name
+    puts position+1
+  else
+    puts "The line is currently empty."
+  end
 end
